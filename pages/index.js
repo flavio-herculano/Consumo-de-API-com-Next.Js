a96a8212e278511bd2a29cde8142a9c563dc1466
Flávio Herculano <flavio.hercullano@gmail.com>
2022-06-29 04:06:52 -0300

    adicionado lista de card ao index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "../components/Head"
+import List from "../components/List"
 
 export default function Home() {
+
   return (
     <div>
       <Head/>
+      <List/>
     </div>
   )
 }
+
