8a4fe615539ce56fedf39dba30bd3dca6f8ab8bc
Flávio Herculano <flavio.hercullano@gmail.com>
2022-06-30 07:30:55 -0300

    adicionado conteudo ao footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,28 @@ import styles from "../styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Rodapé</p>
+      <div>
+        <p>
+          Developed by, <br /> Flávio Herculano de Souza.
+        </p>
+      </div>
+
+      <div className={styles.divWhats}>
+        <img src="images/whatsapp.png" alt="" height={24} />
+        <a
+          className={styles.myNumber}
+          href="https://wa.me/5511988886542?text=Ol%C3%A1+Fl%C3%A1vio."
+        >
+          11 9 8888-6542
+        </a>
+        <p>-</p>
+        <a
+          className={styles.myNumber}
+          href="mailto:flavio.hercullano@gmail.com"
+        >
+          Flavio.hercullano@gmail.com
+        </a>
+      </div>
     </footer>
   );
 }
