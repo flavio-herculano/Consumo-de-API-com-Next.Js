a351561e39073b010f6af927930293f24592838f
Flávio Herculano <flavio.hercullano@gmail.com>
2022-06-30 04:40:47 -0300

    adicionado navbar a todas paginas

---
+++ components/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import styles from "../styles/head.module.css";
+
+export default class Head extends Component {
+  render() {
+    return (
+      <header className={styles.header}>
+        <div className={styles.content}>
+          <img src="/images/logo-imob-520.png" alt="imob-logo" height={32} />
+
+          <div className={styles.divMenuItems}>
+            <div className={styles.divWhats}>
+              <img src="images/whatsapp.png" alt="" height={24} />
+              <a
+                className={styles.myNumber}
+                href="https://wa.me/5511988886542?text=Ol%C3%A1+Fl%C3%A1vio."
+              >
+                11 9 8888-6542
+              </a>
+            </div>
+            <img src="/images/hamburg.png" alt="" />
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
