f723fd022149bcf949bee08d8bde4056382986e3
Flávio Herculano <flavio.hercullano@gmail.com>
2022-06-30 05:50:03 -0300

    Atualizado nome rodapé

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from "../styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Nosso Footer!</p>
+      <p>Rodapé</p>
     </footer>
   );
 }
